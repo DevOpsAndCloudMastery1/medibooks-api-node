104c018b52d7472d6c0972a28e53f9ff249710f2
dhanya <dhanyadhilip191@gmail.com>
2025-05-23 21:38:43 +0200

    Add GET /api/doctors/:id route for getDoctorDetails in doctorRoutes

--- src/routes/doctorRoutes.js
+++ src/routes/doctorRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllDoctors } = require('../controllers/doctorController');
+const { getAllDoctors, getDoctorDetails } = require('../controllers/doctorController');
 
 // Route: GET /api/doctors
 router.get('/', getAllDoctors);
+
+// Route: GET /api/doctors/:id
+router.get('/:id', getDoctorDetails);
 
 module.exports = router;
