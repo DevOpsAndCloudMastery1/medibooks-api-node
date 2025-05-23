1a76e70f003959887e813983bf56cf80bb653eef
dhanya <dhanyadhilip191@gmail.com>
2025-05-23 21:40:51 +0200

    Add getDoctorDetails function to doctorController to handle fetching doctor by ID in doctorController.js

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -1,8 +1,19 @@
-const { getDoctors } = require('../utils/doctorStore');
+const { getDoctors, getDoctorById } = require('../utils/doctorStore');
 
 const getAllDoctors = (req, res) => {
   const doctors = getDoctors();
    res.json(doctors); 
 };
 
-module.exports = { getAllDoctors };
+const getDoctorDetails = (req, res) => {
+  const id = req.params.id;
+  const doctor = getDoctorById(id);
+  
+  if (!doctor) {
+    return res.status(404).json({ message: 'Doctor not found' });
+  }
+
+  res.json(doctor); // 👈 send only that doctor's details
+};
+
+module.exports = { getAllDoctors, getDoctorDetails };
