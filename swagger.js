ceadeeaae85846841fcb44637b2d75238f162013
dhanya <dhanyadhilip191@gmail.com>
2025-06-02 11:20:55 +0200

    feat: Integrate Swagger UI for API testing and documentation

---
+++ swagger.js
@@ -0,0 +1,28 @@
+// swagger.js
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'MediBooks Backend API',
+      version: '1.0.0',
+      description: 'API documentation for the MediBooks application',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Path to your route files
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+
+const setupSwagger = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+module.exports = setupSwagger;
